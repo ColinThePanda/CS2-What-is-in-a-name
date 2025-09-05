34694fe9989c129fae0ef58609596311aa566367
ColinThePanda <urboycolinthepanda@gmail.com>
2025-09-05 08:16:24 -0400

    use msvcrt and format

--- main.py
+++ main.py
@@ -4,137 +4,160 @@ from typing import Dict, Callable
 from collections import Counter
 from string_util import list_to_str, remove_char, upper, lower, split_spaces
 
+
 def reverse(name: str) -> str:
     return list_to_str(reversed(name))
 
-def num_vowels(name : str) -> int:
+
+def num_vowels(name: str) -> int:
     counter = Counter(lower(remove_char(name, " ")))
-    num_vowels = sum([counter.get(vowel, 0) for vowel in ['a', 'e', 'i', 'o', 'u']])
+    num_vowels = sum([counter.get(vowel, 0) for vowel in ["a", "e", "i", "o", "u"]])
     return num_vowels
 
-def consonant_frequency(name : str):
+
+def consonant_frequency(name: str):
     vowels = num_vowels(name)
     return vowels / len(name) / vowels
 
-def split_names(name : str) -> list[str]:
+
+def split_names(name: str) -> list[str]:
     names = split_spaces(name)
-    names = list(filter(lambda name : name != "" and name != None, names))
+    names = list(filter(lambda name: name != "" and name != None, names))
     return names
 
-def first_name(name : str) -> str:
+
+def first_name(name: str) -> str:
     return split_names(name)[0]
 
-def last_name(name : str) -> str:
+
+def last_name(name: str) -> str:
     return split_names(name)[-1]
 
-def middle_names(name : str) -> str | list[str]:
-    middle_names : list[str] = split_names(name)[1:-1]
+
+def middle_names(name: str) -> str | list[str]:
+    middle_names: list[str] = split_names(name)[1:-1]
     middle_names = middle_names[0] if len(middle_names) == 1 else middle_names
     return middle_names if len(middle_names) > 0 else None
 
-def contains_hyphen(name : str) -> bool:
+
+def contains_hyphen(name: str) -> bool:
     return "-" in name
 
-def lowercase(name : str) -> str:
+
+def lowercase(name: str) -> str:
     return lower(name)
 
-def uppercase(name : str) -> str:
+
+def uppercase(name: str) -> str:
     return upper(name)
 
-def mix_up_letters(name : str) -> str:
+
+def mix_up_letters(name: str) -> str:
     pass
 
-def is_palindrome(name : str) -> bool:
+
+def is_palindrome(name: str) -> bool:
     return name == reverse(name)
 
-def sort_name(name : str) -> str:
+
+def sort_name(name: str) -> str:
     return list_to_str(sorted(name))
 
-def initials(name : str) -> str:
+
+def initials(name: str) -> str:
     names = split_names(name)
     initials = [name[0] for name in names]
     return list_to_str(initials)
 
-def contains_title(name : str) -> bool:
-    titles = ['dr', 'phd', 'sir', 'esq']
-    return any([title in lower(remove_char(remove_char(name, " "), ".")) for title in titles])
+
+def contains_title(name: str) -> bool:
+    titles = ["dr", "phd", "sir", "esq"]
+    return any(
+        [title in lower(remove_char(remove_char(name, " "), ".")) for title in titles]
+    )
 
 
 def menu() -> int:
-    selections = ["1. Reverse",
-     "2. Num of Vowels",
-     "3. Consonant Frequency",
-     "4. First Name",
-     "5. Last Name",
-     "6. Middle Name(s)",
-     "7. Contains Hyphen",
-     "8. Lowercase",
-     "9. Upercase",
-     "10. Mix Up Letters",
-     "11. Is Palindrome",
-     "12. Sorted",
-     "13. Initials",
-     "14. Contains Title"]
-    
+    selections = [
+        "1. Reverse",
+        "2. Num of Vowels",
+        "3. Consonant Frequency",
+        "4. First Name",
+        "5. Last Name",
+        "6. Middle Name(s)",
+        "7. Contains Hyphen",
+        "8. Lowercase",
+        "9. Upercase",
+        "10. Mix Up Letters",
+        "11. Is Palindrome",
+        "12. Sorted",
+        "13. Initials",
+        "14. Contains Title",
+    ]
+
     selection = 1
     running = True
-    
-    while running:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        for i, choice in enumerate(selections):
-            print(f"\033[32m{choice}\033[0m" if selection == i+1 else choice)
-        
-        while not msvcrt.kbhit():
-            pass
-        
-        key_byte = msvcrt.getch()
 
-        if key_byte ==  b'\xe0': # arrow key
-            arrow_key = msvcrt.getch()
-            match arrow_key:
-                case b'H': # up
-                    selection -= 1
-                case b'P': # down
-                    selection += 1
-                case b' ':
-                    break
-        elif key_byte == b' ':
-            break
-        
+    while running:
+        os.system("cls" if os.name == "nt" else "clear")
+        for i, choice in enumerate(selections):
+            print(f"\033[42m{choice}\033[0m" if selection == i + 1 else choice)
+
+        action_made = False
+        while not action_made:
+            while not msvcrt.kbhit():
+                pass
+
+            key_byte = msvcrt.getch()
+
+            if key_byte == b"\xe0":  # arrow key
+                arrow_key = msvcrt.getch()
+                match arrow_key:
+                    case b"H":  # up
+                        selection -= 1
+                        action_made = True
+                    case b"P":  # down
+                        selection += 1
+                        action_made = True
+            elif key_byte == b" " or key_byte == b"\r":
+                action_made = True
+                running = False
+
         if selection < 1:
             selection = len(selections)
         if selection > len(selections):
             selection = 1
-    
+
     return selection
 
 
 def main():
-    selection : int = menu()
-    os.system('cls' if os.name == 'nt' else 'clear')
-    name : str = input("What is your name?")
-    
-    function_key : Dict[Callable] = {
-        1 : reverse,
-        2 : num_vowels,
-        3 : consonant_frequency,
-        4 : first_name,
-        5 : last_name,
-        6 : middle_names,
-        7 : contains_hyphen,
-        8 : lowercase,
-        9 : uppercase,
-        10 : mix_up_letters,
-        11 : is_palindrome,
-        12 : sort_name,
-        13 : initials,
-        14 : contains_title
+    selection: int = menu()
+    os.system("cls" if os.name == "nt" else "clear")
+    name: str = input("What is your name?")
+
+    function_key: Dict[Callable] = {
+        1: reverse,
+        2: num_vowels,
+        3: consonant_frequency,
+        4: first_name,
+        5: last_name,
+        6: middle_names,
+        7: contains_hyphen,
+        8: lowercase,
+        9: uppercase,
+        10: mix_up_letters,
+        11: is_palindrome,
+        12: sort_name,
+        13: initials,
+        14: contains_title,
     }
-    
+
     function = function_key.get(selection)
-    function_call = lambda name : function(name)
+    function_call = lambda name: function(name)
     result = function_call(name)
     print(result)
+
 
 if __name__ == "__main__":
     main()
